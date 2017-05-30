28a1b4d4b48aaff7fc798bf4ba5fd5b415045c1f
ArtixAllMighty <robbeenaxel@hotmail.com>
2017-05-30 11:10:16 +0200

    added actual code to the packet ...

--- lib/playerclass/network/CPacketSyncPlayerClass.java
+++ lib/playerclass/network/CPacketSyncPlayerClass.java
@@ -1,27 +1,32 @@
 package lib.playerclass.network;
 
 import io.netty.buffer.ByteBuf;
+import lib.playerclass.capability.PlayerClass;
 import net.minecraft.client.Minecraft;
+import net.minecraftforge.fml.common.network.ByteBufUtils;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
 public class CPacketSyncPlayerClass implements IMessage{
 
+	String classname;
+	
 	public CPacketSyncPlayerClass() {
 	}
 
 	public CPacketSyncPlayerClass(String playerClassname) {
+		classname = playerClassname;
 	}
 	
 	@Override
 	public void fromBytes(ByteBuf buf) {
-		
+		classname = ByteBufUtils.readUTF8String(buf);
 	}
 
 	@Override
 	public void toBytes(ByteBuf buf) {
-		
+		ByteBufUtils.writeUTF8String(buf, classname);
 	}
 	
 	public static class CPacketSyncPlayerClassHandler implements IMessageHandler<CPacketSyncPlayerClass, IMessage>
@@ -30,7 +35,10 @@ public class CPacketSyncPlayerClass implements IMessage{
 		@Override
 		public IMessage onMessage(CPacketSyncPlayerClass message, MessageContext ctx) {
 				Minecraft.getMinecraft().addScheduledTask(()->{
-					
+					if(message.classname != null)
+					{
+						PlayerClass.get(Minecraft.getMinecraft().player).setPlayerClass(message.classname);
+					}
 				});
 			return null;
 		}
